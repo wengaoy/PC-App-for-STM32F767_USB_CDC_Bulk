de35cd3962758b4a67f8ae7f6ef45c7ef6111f1e
wengaoy <wengaoy@yahoo.com>
2022-09-22 16:04:10 -0400

    Add project files.

---
+++ STM32F767_USB_CDC_Bulk/Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿
+namespace STM32F767_USB_CDC_Bulk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnWrite_ReadTest = new System.Windows.Forms.Button();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.lblUSBstatus = new System.Windows.Forms.ToolStripStatusLabel();
+            this.txtinfo = new System.Windows.Forms.TextBox();
+            this.btnOpen = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnWrite_ReadTest
+            // 
+            this.btnWrite_ReadTest.Location = new System.Drawing.Point(25, 86);
+            this.btnWrite_ReadTest.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnWrite_ReadTest.Name = "btnWrite_ReadTest";
+            this.btnWrite_ReadTest.Size = new System.Drawing.Size(152, 54);
+            this.btnWrite_ReadTest.TabIndex = 0;
+            this.btnWrite_ReadTest.Text = "write read test";
+            this.btnWrite_ReadTest.UseVisualStyleBackColor = true;
+            this.btnWrite_ReadTest.Click += new System.EventHandler(this.btnWrite_ReadTest_Click);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(24, 24);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.lblUSBstatus});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 513);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Padding = new System.Windows.Forms.Padding(3, 0, 32, 0);
+            this.statusStrip1.Size = new System.Drawing.Size(927, 36);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // lblUSBstatus
+            // 
+            this.lblUSBstatus.BorderSides = System.Windows.Forms.ToolStripStatusLabelBorderSides.Right;
+            this.lblUSBstatus.Name = "lblUSBstatus";
+            this.lblUSBstatus.Size = new System.Drawing.Size(120, 29);
+            this.lblUSBstatus.Text = "USBIO Status";
+            // 
+            // txtinfo
+            // 
+            this.txtinfo.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.txtinfo.Location = new System.Drawing.Point(0, 232);
+            this.txtinfo.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtinfo.Multiline = true;
+            this.txtinfo.Name = "txtinfo";
+            this.txtinfo.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.txtinfo.Size = new System.Drawing.Size(927, 281);
+            this.txtinfo.TabIndex = 3;
+            // 
+            // btnOpen
+            // 
+            this.btnOpen.Location = new System.Drawing.Point(25, 26);
+            this.btnOpen.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnOpen.Name = "btnOpen";
+            this.btnOpen.Size = new System.Drawing.Size(152, 54);
+            this.btnOpen.TabIndex = 4;
+            this.btnOpen.Text = "Open";
+            this.btnOpen.UseVisualStyleBackColor = true;
+            this.btnOpen.Click += new System.EventHandler(this.btnOpen_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(25, 150);
+            this.btnClose.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(152, 54);
+            this.btnClose.TabIndex = 5;
+            this.btnClose.Text = "Close";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(210, 104);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(320, 20);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "Send byte 0-63 and will read back the same ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(927, 549);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnOpen);
+            this.Controls.Add(this.txtinfo);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.btnWrite_ReadTest);
+            this.Name = "Form1";
+            this.Text = "Stm32F767Zi USB CDC Bulk transfer Test";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnWrite_ReadTest;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel lblUSBstatus;
+        private System.Windows.Forms.TextBox txtinfo;
+        private System.Windows.Forms.Button btnOpen;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ STM32F767_USB_CDC_Bulk/Form1.cs
@@ -0,0 +1,222 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using LibUsbDotNet;
+using LibUsbDotNet.Info;
+using LibUsbDotNet.Main;
+using System.Collections.ObjectModel;
+using LibUsbDotNet.LudnMonoLibUsb;
+using System.Threading;
+using LibUsbDotNet.DeviceNotify;
+using LibUsbDotNet.Descriptors;
+
+namespace STM32F767_USB_CDC_Bulk
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public UsbDevice MyUsbDevice;
+        public UsbDeviceFinder MyUsbFinder = new UsbDeviceFinder(0x0483, 0xC1B0);
+        public UsbEndpointWriter writer;
+        UsbEndpointReader reader;// = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01, 64);//ep01
+
+        #region usb function
+        public bool WriteData(byte[] buffer)
+        {
+            ErrorCode ec = ErrorCode.None;
+
+            try
+            {
+                // open write endpoint 1.
+                // UsbEndpointWriter writer = MyUsbDevice.OpenEndpointWriter(WriteEndpointID.Ep01, EndpointType.Bulk);
+                int bytesWritten;   //number of bytes written
+                ec = writer.Write(buffer, 2000, out bytesWritten);//.Write(testbyte, 2000, out 2);
+
+                Console.WriteLine("writing was " + ec + " byte writen: " + bytesWritten);
+                return true;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show((ec != ErrorCode.None ? ec + ":" : String.Empty) + ex.Message);
+                return false;
+            }
+
+        }
+        public void ReadData(byte[] readBuffer)
+        {
+            ErrorCode ec = ErrorCode.None;
+
+            try
+            {
+                // open read endpoint ep03.
+                // UsbEndpointReader reader = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01, 64);//ep01
+                while (ec == ErrorCode.None)
+                {
+                    int bytesRead;
+
+                    // If the device hasn't sent data in the last 100 milliseconds,
+                    // a timeout error (ec = IoTimedOut) will occur. 
+                    //  ec = reader.Read(readBuffer, 100, out bytesRead);
+
+                    ec = reader.Read(readBuffer, 400, out bytesRead);
+
+                    if (bytesRead == 0) throw new Exception("No more bytes!");
+
+
+                    // Write that output to the console.
+                    // Console.Write(Encoding.Default.GetString(readBuffer, 0, bytesRead));
+
+                    //foreach (byte a in readBuffer)
+                    //{
+                    //    Console.WriteLine("ReadBuffer: " + a);
+
+                    //}
+                    //Console.WriteLine("byteRead: " + bytesRead + "\r\n");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine();
+                Console.WriteLine((ec != ErrorCode.None ? ec + ":" : String.Empty) + ex.Message + " nothing to read.");
+            }
+        }
+
+        public int OpenDev(int Dev)
+        {
+            ErrorCode ec = ErrorCode.None;
+            if (Dev == 0)
+            {
+                try
+                {
+                    // Find and open the usb device.
+                    MyUsbDevice = UsbDevice.OpenUsbDevice(MyUsbFinder);
+
+                    // If the device is open and ready
+                    if (MyUsbDevice == null) throw new Exception("Device Not Found.");
+
+                    IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                    if (!ReferenceEquals(wholeUsbDevice, null))
+                    {
+                        // Select config #1
+                        wholeUsbDevice.SetConfiguration(1);
+
+                        // Claim interface #0.
+                        wholeUsbDevice.ClaimInterface(0);//can do the same interface 0 and 1????
+                        ////set Alt interface
+                        //wholeUsbDevice.SetAltInterface(1);
+
+                        writer = MyUsbDevice.OpenEndpointWriter(WriteEndpointID.Ep01, EndpointType.Bulk);
+                        reader = MyUsbDevice.OpenEndpointReader(ReadEndpointID.Ep01, 64);
+                    }
+                    return Dev;
+
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine((ec != ErrorCode.None ? ec + ":" : String.Empty) + ex.Message);
+                    //MessageBox.Show((ec != ErrorCode.None ? ec + ":" : String.Empty) + ex.Message);
+                    // Release interface #0.
+                    //wholeUsbDevice.ReleaseInterface(0);
+                    return -1;
+                }
+
+            }
+            else
+            {
+                Console.WriteLine("Only support single USBIO Device!");
+                //MessageBox.Show("Only support single USBIO Device!");
+                return -1;
+            }
+
+
+        }
+        public void CloseDev()
+        {
+            if (MyUsbDevice != null)
+            {
+                if (MyUsbDevice.IsOpen)
+                {
+                    // If this is a "whole" usb device (libusb-win32, linux libusb-1.0)
+                    // it exposes an IUsbDevice interface. If not (WinUSB) the 
+                    // 'wholeUsbDevice' variable will be null indicating this is 
+                    // an interface of a device; it does not require or support 
+                    // configuration and interface selection.
+                    IUsbDevice wholeUsbDevice = MyUsbDevice as IUsbDevice;
+                    if (!ReferenceEquals(wholeUsbDevice, null))
+                    {
+                        // Release interface #0.
+                        wholeUsbDevice.ReleaseInterface(0);
+                    }
+
+                    MyUsbDevice.Close();
+                }
+                MyUsbDevice = null;
+
+                // Free usb resources
+                UsbDevice.Exit();
+
+            }
+        }
+        #endregion
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnWrite_ReadTest_Click(object sender, EventArgs e)
+        {
+            byte[] dataW = new byte[64];
+            byte[] dataR = new byte[64];
+
+            for (byte i = 0; i < 64; i++)
+            {
+                dataW[i] = i;
+            }
+
+            txtinfo.AppendText("Sending byte 0-63\r\n");
+
+            WriteData(dataW);
+            Thread.Sleep(500);
+            ReadData(dataR);
+
+            //CloseDev();
+
+            txtinfo.AppendText("Reading ... \r\n");
+            foreach (byte i in dataR)
+            {
+                txtinfo.AppendText(i.ToString() + " ");
+            }
+            txtinfo.AppendText("done\r\n");
+        }
+
+        private void btnOpen_Click(object sender, EventArgs e)
+        {
+            if (OpenDev(0) == 0)
+            {
+                lblUSBstatus.Text = "STM32 USBIO found!";         
+            }
+            else
+            {
+                lblUSBstatus.Text = "STM32 USBIO not found!";
+            }
+        }
+
+        private void btnClose_Click(object sender, EventArgs e)
+        {
+            CloseDev();
+        }
+    }
+}
